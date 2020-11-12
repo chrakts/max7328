a9528217a072272404d9cdef00278f683c2232d1
Christof <chrak@cmja.de>
2020-11-12 22:11:11 +0100

    Erstausgabe

---
+++ max7328.cpp
@@ -0,0 +1,96 @@
+/*
+ * CPPFile1.cpp
+ *
+ * Created: 01.01.2016 13:31:08
+ *  Author: Christof
+ */
+
+
+#include "max7328.h"
+
+
+MAX7328::MAX7328(void)
+{
+}
+
+MAX7328::MAX7328(TWI_MasterDriver_t *mytwi,int address)
+{
+	twi = mytwi;
+	twiAddress = address;
+	getGPIO();
+	_PIN = 0;
+	_PORT = 0;
+	_DDR = 0;
+}
+
+void MAX7328::setOutput(uint8_t pin)
+{
+  _PORT |= (1 << pin);
+}
+
+void MAX7328::clearOutput(uint8_t pin)
+{
+  _PORT &= ~(1 << pin);
+}
+
+
+void MAX7328::pinMode(uint8_t pin, uint8_t mode)
+{
+	/* Switch according mode */
+	switch (mode) {
+	case INPUT:
+		_DDR &= ~(1 << pin);
+		_PORT &= ~(1 << pin);
+		break;
+
+	case INPUT_PULLUP:
+		_DDR &= ~(1 << pin);
+		_PORT |= (1 << pin);
+		break;
+
+	case OUTPUT:
+		_DDR |= (1 << pin);
+		_PORT &= ~(1 << pin);
+		break;
+
+	default:
+		break;
+	}
+
+	/* Update GPIO values */
+	updateGPIO();
+}
+
+void MAX7328::updateGPIO()
+{
+
+	/* Read current GPIO states */
+	//readGPIO(); // Experimental
+
+	/* Compute new GPIO states */
+	//uint8_t value = ((_PIN & ~_DDR) & ~(~_DDR & _PORT)) | _PORT; // Experimental
+	//uint8_t value = ( _PIN & (~_DDR) ) | _PORT;
+	uint8_t value =  _PORT;
+
+	/* Start communication and send GPIO values as byte */
+	//twi->beginTransmission(_address);
+	//twi->writeData(value);
+	//I2CWRITE(value);
+	//twi->endTransmission();
+	TWI_MasterWrite(twi,twiAddress,&value,1);
+}
+
+void MAX7328::newValue(uint8_t val)
+{
+  TWI_MasterWrite(twi,twiAddress,&val,1);
+}
+
+uint8_t MAX7328::getGPIO()
+{
+	TWI_MasterRead(twi,twiAddress,1);
+
+	while (twi->status != TWIM_STATUS_READY) {
+		/* Wait until transaction is complete. */
+	}
+	return( twi->readData[0] );
+}
---
+++ max7328.h
@@ -0,0 +1,57 @@
+/*
+ * max7328.h
+ *
+ * Created: 01.01.2016 13:35:13
+ *  Author: Christof
+ */
+
+
+#ifndef MAX7328_H_
+#define MAX7328_H_
+
+#include <avr/io.h>
+#include <stdbool.h>
+#include <avr/interrupt.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include "twi_master_driver.h"
+
+class MAX7328
+{
+	private:
+
+	TWI_MasterDriver_t *twi;
+	uint8_t	twiAddress;
+	uint16_t raw_data;
+	double illuminance;
+	protected:
+	/** Output pins values */
+	volatile uint8_t _PORT;
+
+	/** Current input pins values */
+	volatile uint8_t _PIN;
+
+	/** Pins modes values (OUTPUT or INPUT) */
+	volatile uint8_t _DDR;
+
+	/** PCF8574 I2C address */
+	uint8_t _address;
+
+	public:
+  enum {INPUT=0,INPUT_PULLUP,OUTPUT};
+	MAX7328();
+	MAX7328(TWI_MasterDriver_t *mytwi,int address);
+  void pinMode(uint8_t pin, uint8_t mode);
+  void updateGPIO();
+  void setOutput(uint8_t pin);
+  void clearOutput(uint8_t pin);
+
+  void newValue(uint8_t val);
+	uint8_t getGPIO();
+
+};
+
+
+#endif /* MAX7328_H_ */
